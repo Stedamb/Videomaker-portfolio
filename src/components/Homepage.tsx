fa592d10fb7189101b6eb10c5a89ebf738b35929
stedamb <stedamb@gmail.com>
2024-08-08 15:36:36 +0000

    text balance in homepage

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     return (
         <div className="container-xl">
             <div className="container-sm">
-            <p className="text-center pt-8 pb-8">Un video-diario dei miei viaggi, avventure e qualche esperimento di videomaking. </p>
+            <p className="text-center text-balance pt-8 pb-8">Un video-diario dei miei viaggi, avventure e qualche esperimento di videomaking. </p>
             </div>
             <GridSection videos={videos}></GridSection>
         </div>
