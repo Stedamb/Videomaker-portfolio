b9d4eb16f570394a052a0b86386bfb5583cd277a
stedamb <stedamb@gmail.com>
2024-08-08 14:26:21 +0000

    page loader small fixes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,44 +1,48 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Cookies from 'js-cookie';
 
 const Loader = () => {
   const loaderRef = useRef<HTMLDivElement>(null);
-  const [showLoader, setShowLoader] = useState(true);
+  const h3Ref = useRef<HTMLHeadingElement>(null);
+  const redCircleRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const hasSeenLoader = Cookies.get('loaderSeen');
-    if (hasSeenLoader) {
-      setShowLoader(false);
+    const loader = loaderRef.current;
+    const h3 = h3Ref.current;
+    const redCircle = redCircleRef.current;
+
+    if (hasSeenLoader && loader) {
+      loader.classList.add('hide');
     } else {
       Cookies.set('loaderSeen', 'true', { expires: 1 });
-    }
 
-    const loader = loaderRef.current;
-    if (loader) {
-      loader.classList.remove("hide");
-      
-      setTimeout(() => {
+      if (h3) h3.classList.remove('hide');
+      if (redCircle) redCircle.classList.remove('hide');
+
+      const hideLoaderTimeout = setTimeout(() => {
         if (loader) {
-          loader.classList.add("hide");
+          loader.classList.add('hide');
         }
       }, 1500);
 
-      setTimeout(() => {
+      const removeLoaderTimeout = setTimeout(() => {
         if (loader) {
-          loader.classList.remove("flex");
-          loader.classList.add("hidden");
+          // loader.remove();
         }
-      }, 2000);
-    }
+      }, 3000);
+
+      return () => {
+        clearTimeout(hideLoaderTimeout);
+        clearTimeout(removeLoaderTimeout);
+      };    }
   }, []);
 
   return (
-    showLoader && (
-      <div ref={loaderRef} className="Loader hide flex items-center justify-center">
-        <h3>REC</h3>
-        <div className="red-circle"></div>
-      </div>
-    )
+    <div ref={loaderRef} className="Loader flex items-center justify-center">
+      <h3 ref={h3Ref} className="hide">REC</h3>
+      <div ref={redCircleRef} className="hide red-circle"></div>
+    </div>
   );
 };
 
